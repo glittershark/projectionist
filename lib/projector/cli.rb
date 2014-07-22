be5d871036f1c15b17db90834e39e810d939a522
Griffin Smith <wildgriffin45@gmail.com>
2014-07-22 10:35:59 -0400

    Make CLI use Projections#file_for

--- lib/projector/cli.rb
+++ lib/projector/cli.rb
@@ -7,11 +7,15 @@ module Projector
 
     def initialize(*)
       @projections = Projector::Projections.new
+      @projections.load_file
       super
     end
 
     desc 'edit <type> <file>', 'Edit the file for <type> named <file>'
     def edit(type, file)
+      editor = ENV['EDITOR'] || `which vim`
+      file = @projections.file_for type, file
+      Kernel.exec "#{editor} #{file}"
     end
   end
 end
