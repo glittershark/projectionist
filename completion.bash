237b90326cafaa99ac457bd742af0e7d2065056e
Griffin Smith <wildgriffin45@gmail.com>
2014-11-17 20:19:38 -0500

    Convert indentation in completion.bash to spaces

--- completion.bash
+++ completion.bash
@@ -1,30 +1,29 @@
 #!/bin/bash
 
-_prj()
-{
+_prj() {
     local cur prev opts
     COMPREPLY=()
     cur="${COMP_WORDS[COMP_CWORD]}"
     prev="${COMP_WORDS[COMP_CWORD-1]}"
     cmds="edit help list types"
 
-	case $prev in
-		prj | help)
-			COMPREPLY=( $(compgen -W "${cmds}" -- ${cur}) )
-			return 0
-			;;
-		edit | types)
-			types=$(prj types)
-			COMPREPLY=( $(compgen -W "${types}" -- ${cur}) )
-			;;
-		*)
-			# Test if we're editing a type
-			if [ "${COMP_WORDS[COMP_CWORD-2]}" = 'edit' ]; then
-				files=$(prj list ${prev})
-				COMPREPLY=( $(compgen -W "${files}" -- ${cur}) )
-			fi
-			;;
-	esac
+    case $prev in
+        prj | help)
+            COMPREPLY=( $(compgen -W "${cmds}" -- ${cur}) )
+            return 0
+            ;;
+        edit | types)
+            types=$(prj types)
+            COMPREPLY=( $(compgen -W "${types}" -- ${cur}) )
+            ;;
+        *)
+            # Test if we're editing a type
+            if [ "${COMP_WORDS[COMP_CWORD-2]}" = 'edit' ]; then
+                files=$(prj list ${prev})
+                COMPREPLY=( $(compgen -W "${files}" -- ${cur}) )
+            fi
+            ;;
+    esac
 }
 complete -F _prj prj
 
